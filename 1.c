f18a082e71d9cb9d70f8854d3d74751b31087a09
Yashrajsinghcoder <101283499+Yashrajsinghcoder@users.noreply.github.com>
2022-03-10 22:30:23 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,4 +1,33 @@
 #include<stdio.h>
-int main(){
-    printf("pratik chutiya");
+#include<stdlib.h>
+#include<string.h>
+
+void menu()
+{
+    printf("\t\t\t\t\t\t*****FRUIT JUICE MENU*****\n");
+    printf("\n");
+    printf("\t\t\t\tH(In Rs.)\tF(In Rs.)\t\t\t\t\t\tH(In Rs.)\tF(In Rs.)\n");
+    printf("1. Lime/Soda\t\t--\t  20.00\t\t 30.00\t\t16. Pomogranate Pizzaz\t\t--\t  60.00\t\t 100.00\n");
+    printf("2. Papaya\t\t--\t  25.00\t\t 40.00\t\t17. Strawberry Grape Orange\t--\t  60.00\t\t 100.00\n");
+    printf("3. Beet root\t\t--\t  25.00\t\t 40.00\t\t18. Golden spice\t\t--\t  60.00\t\t 100.00\n");
+    printf("4. Banana\t\t--\t  25.00\t\t 40.00\t\t19. Pink Panther\t\t--\t  60.00\t\t 100.00\n");
+    printf("5. Carrot\t\t--\t  25.00\t\t 40.00\t\t20. Romaine Apple Orange\t--\t  60.00\t\t 100.00\n");
+    printf("6. Mosumbi\t\t--\t  30.00\t\t 50.00\t\t21. Sunset Blend\t\t--\t  60.00\t\t 100.00\n");
+    printf("7. Cantaloupe\t\t--\t  30.00\t\t 50.00\t\t22. Beet A-retreat\t\t--\t  50.00\t\t  90.00\n");
+    printf("8. Sapota\t\t--\t  30.00\t\t 50.00\t\t23. Berry A Peeling\t\t--\t  50.00\t\t  90.00\n");
+    printf("9. Grape\t\t--\t  30.00\t\t 50.00\t\t24. Apple Cucumber\t\t--\t  50.00\t\t  90.00\n");
+    printf("10.Watermelon\t\t--\t  30.00\t\t 50.00\t\t25. Pomogranate Apple Juice\t--\t  50.00\t\t  90.00\n");
+    printf("11.Pineapple\t\t--\t  35.00\t\t 60.00\t\t26. Grapes Orange\t\t--\t  50.00\t\t  90.00\n");
+    printf("12.Mango(Seasonal)\t--\t  40.00\t\t 70.00\t\t27. Strawberry Pineapple Orange\t--\t  50.00\t\t  90.00\n");
+    printf("13.Orange\t\t--\t  40.00\t\t 70.00\t\t28. Green Ginger Ale\t\t--\t  50.00\t\t  90.00\n");
+    printf("14.Pomegranate\t\t--\t  45.00\t\t 80.00\t\t29. Mexicon Style Jugo\t\t--\t  50.00\t\t  90.00\n");
+    printf("15.Apple\t\t--\t  50.00\t\t 80.00\t\t30. Strawberry Pineaplle\t--\t  50.00\t\t  90.00\n");
+
 }
+
+int main()
+{
+    menu();
+    return 0;
+}
+    
